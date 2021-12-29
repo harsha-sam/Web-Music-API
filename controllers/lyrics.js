57e074494d7536b3c11d422ef54d336ceeca5c62
Harsha <harsha.sam23@gmail.com>
2021-12-29 13:49:19 +0530

    lyrics API error

--- controllers/lyrics.js
+++ controllers/lyrics.js
@@ -1,19 +1,22 @@
 const geniusLyricsApi = require("genius-lyrics-api")
 
 const handleLyricsGet = async (req, res) => {
-    const options = {
-        apiKey: process.env.GENIUS_API_KEY,
-        title: req.query.title,
-        artist: req.query.artist, 
-        optimizeQuery: true
-    };
-    geniusLyricsApi.getLyrics(options).then((lyrics) => {
-        if (!lyrics) res.json({ lyrics: "No lyrics found !" })
-        else {
-          res.json({ lyrics })
-        }
+  const options = {
+    apiKey: process.env.GENIUS_API_KEY,
+    title: req.query.title,
+    artist: req.query.artist,
+    optimizeQuery: true
+  };
+  geniusLyricsApi.getLyrics(options).then((lyrics) => {
+    if (!lyrics) res.json({ lyrics: "No lyrics found !" })
+    else {
+      res.json({ lyrics })
+    }
+  })
+    .catch((err) => {
+      console.log(err, "error")
+      res.json({ lyrics: "No lyrics found !" })
     })
-    .catch((err) => res.json({ lyrics: "No lyrics found !" }))
 }
 module.exports={
     handleLyricsGet
